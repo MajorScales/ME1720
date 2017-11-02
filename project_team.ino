f1a2a030ee81ad57d7bec676ba783687f9351fb1
Davonp <33331340+Davonp@users.noreply.github.com>
2017-11-02 15:41:23 -0500

    Update project_team.ino

--- project_team.ino
+++ project_team.ino
@@ -1,17 +1,19 @@
 // Pargeon Davon October 10/24/2017
 // A program to take no force, and start a motor. Then take a photo receptor to start a motor.
 
-const int DIR_AA = 5;
-const int DIR_BA = 4;
-const int PWM_A = 6;
-const int DIR_AB = 8;
-const int DIR_BB = 9;
-const int PWM_B = 10;
+const int DIR_AA = 5; // Direction Pin 1 for motor a
+const int DIR_BA = 4; // Direction pin 2 for motor a
+const int PWM_A = 6;  // Power Pin for  motor a
+const int DIR_AB = 8; // Direction pin 1 for motor b
+const int DIR_BB = 9; // Direction pin 2 for motor b
+const int PWM_B = 10; // Power pin motor b
 
 // Sensors
 
-const int SUN = A1;
+const int SUN = A1; 
 const int FORCE = A0;
+
+// Initalizing variables for sensor pins
 int force = 0;
 int solar = 0;
 
@@ -45,13 +47,15 @@ void loop(){
 
 
   // put your main code here, to run repeatedly:
+  // Prints out valuesa of the pins
 Serial.println (analogRead(A0));
 Serial.println (analogRead(A1));
+  // Pin variables initalizion
 force = analogRead(A0);
 solar = analogRead(A1);
   //Drive forward speed with DIR_AA high, andDIR_BA low
  
-
+// For when the force sensor has force less than one
   if(force < 1)
   {
   digitalWrite(DIR_AA,HIGH);
@@ -66,6 +70,7 @@ solar = analogRead(A1);
 
   delay(1000);
   
+    // For when the solar senosr has light greater than one
   if(solar>1)
  digitalWrite(DIR_AB,HIGH); 
   digitalWrite(DIR_BB, LOW);
